126fd83623652df2f6852ac46f76467cefcb669e
juanrare <juan.ramirez@ceiba.com.co>
2020-11-07 16:59:44 -0500

    segundo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
 /* Populate tables */
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Juan Camilo', 'Ramirez', 'juan.ramirez@uco.net.co', '1996-01-30');
